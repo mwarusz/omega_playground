80adb3bdd381aa6f601754bab5add4e3b3b6b951
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-09-21 17:15:55 -0600

    Improve formatting script

--- apply_clang_format.sh
+++ apply_clang_format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 script_dir=`dirname "$0"`
-for file in `find $script_dir -name \*.hpp -o -name \*.cpp -not -path "*/build/*"`; do 
+for file in `find $script_dir \( -name \*.hpp -or -name \*.cpp \) -not -path "*/build/*"`; do 
 	clang-format --style=llvm -i $file
 done
