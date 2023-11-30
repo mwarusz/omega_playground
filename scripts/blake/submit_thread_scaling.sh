a04c63ce53a8b7096b13901b4d252d32881a47d1
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-11-30 14:48:01 -0700

    Update blake

--- scripts/blake/submit_thread_scaling.sh
+++ scripts/blake/submit_thread_scaling.sh
@@ -9,20 +9,26 @@
 #SBATCH --time=02:00:00
 #SBATCH --output=job-omega-thread-scaling-%j.log
 
+outdir=${1:-"frontier_thread_scaling"}
 script_path=$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}')
 script_dir=$(dirname $script_path)
 
 outdir="blake_results"
 mkdir -p $outdir && cd $outdir
 
-source $script_dir/../../machines/blake-cpu.env
-
 threads="1 2 4 8 16 24 32 40 48 56 64 72 80 88 96"
 nvertlevels=64
 ntracers=1
+ncellsx_base=50
+nsteps_base=512
 
-$script_dir/../run_thread_scaling.sh 50  $nvertlevels $ntracers  256 "$threads"
-$script_dir/../run_thread_scaling.sh 100 $nvertlevels $ntracers  128 "$threads"
-$script_dir/../run_thread_scaling.sh 200 $nvertlevels $ntracers  32  "$threads"
-$script_dir/../run_thread_scaling.sh 400 $nvertlevels $ntracers  8   "$threads"
-$script_dir/../run_thread_scaling.sh 800 $nvertlevels $ntracers  2   "$threads"
+ncellsx=$ncellsx_base
+nsteps=$nsteps_base
+
+for l in {1..5}
+do
+  echo "$ncellsx $nsteps"
+  $script_dir/../run_thread_scaling.sh $ncellsx $nvertlevels $ntracers $nsteps "$threads" $outdir 
+  ncellsx=$((2*$ncellsx))
+  nsteps=$(($nsteps/4))
+done
