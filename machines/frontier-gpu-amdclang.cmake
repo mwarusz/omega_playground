4f44212eb8e693b161982a48041167d582ded7bb
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-24 11:29:38 -0400

    Update frontier files

--- machines/frontier-gpu-amdclang.cmake
+++ machines/frontier-gpu-amdclang.cmake
@@ -1,6 +1,5 @@
 set(CMAKE_CXX_COMPILER "amdclang++" CACHE STRING "")
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
-set(CMAKE_CXX_FLAGS --gcc-toolchain=$ENV{GCC_PATH}/snos CACHE STRING "")
 
 set(Kokkos_ENABLE_SERIAL ON CACHE BOOL "")
 set(Kokkos_ENABLE_OPENMP OFF CACHE BOOL "")
